11bdb2164051c05454f18a509411ba0b4ab03731
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-24 00:28:49 +0800

    Added API Resource

--- src/api/TodoResource.js
+++ src/api/TodoResource.js
@@ -1,13 +1,22 @@
 export default {
-  getAll: () => fetch(
-    "http://localhost:8080/api/todos",
-    {mode: 'cors'}
+    getAll: () => fetch(
+        "http://localhost:8080/api/todos",
+        {mode: 'cors'}
     ),
-  createTodo: (todo) => fetch(
-    "http://localhost:8080/api/todos", {
-      mode: 'cors',
-      method: 'POST',
-      headers: new Headers({'Content-Type': 'application/json'}),
-      body: JSON.stringify(todo)
-    })
+    createTodo: (todo) => fetch(
+        "http://localhost:8080/api/todos", {
+            mode: 'cors',
+            method: 'POST',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify(todo)
+        }),
+    retainCheck: (todo) => fetch(
+        "http://localhost:8080/api/todos/" + todo.id, {
+            mode: 'cors',
+            method: 'PATCH',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({
+                "status": todo.status
+            })
+        })
 }
