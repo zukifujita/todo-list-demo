8352b6cc3a5fd1787aa96df00f3b0835a00249a9
MIZUKI FUJITA (DOCU-IRIS-ISD-OOCLL/MNL) <FUJITMI@OOCL.COM>
2019-10-24 00:30:40 +0800

    Added Reducers

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,29 +1,42 @@
 const initialState = {
-  todos: [],
-  isOnlyActive: false
+    todos: [],
+    isOnlyActive: false
 };
 
-export default (state = initialState, { type, payload }) => {
-  switch (type) {
-    case "ADD_TODO":
-      return {
-        ...state,
-        todos: [...state.todos, payload]
-      };
+export default (state = initialState, {type, payload}) => {
+    switch (type) {
+        case "ADD_TODO":
+            return {
+                ...state,
+                todos: [...state.todos, payload]
+            };
 
-    case "REFRESH_TODOS":
-      return {
-        ...state,
-        todos: payload
-      };
+        case "REFRESH_TODOS":
+            return {
+                ...state,
+                todos: payload
+            };
 
-    case "SET_FILTER":
-      return {
-        ...state,
-        isOnlyActive: payload
-      };
+        case "SET_FILTER":
+            return {
+                ...state,
+                isOnlyActive: payload
+            };
 
-    default:
-      return state
-  }
+        case "RETAIN_CHECK":
+            const newTodos = state.todos.map(todo => {
+                if (todo.id === payload.id) {
+                    return payload;
+                } else {
+                    return todo;
+                }
+            });
+            return {
+                ...state,
+                todos: [...newTodos]
+            }
+
+        default:
+            return state
+    }
 }
